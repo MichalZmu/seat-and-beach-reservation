91aabf3ea953d5b7418af6ed7bf4fade13801011
Lukasz Hapek <lukaszhapek@gmail.com>
2020-05-30 18:39:31 +0200

    Choose place component part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {DefaultModule} from './layouts/default/default.module';
@@ -14,9 +14,10 @@ import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.c
     PageNotFoundComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
-    DefaultModule
+    DefaultModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -4,13 +4,15 @@ import { DefaultComponent } from './default.component';
 import {SharedModule} from '../../shared/shared.module';
 import {RouterModule} from '@angular/router';
 import {HomeComponent} from '../../modules/home/home.component';
+import { ReservationModule } from 'src/app/modules/reservation/reservation.module';
 
 @NgModule({
   declarations: [DefaultComponent, HomeComponent],
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    ReservationModule
   ]
 })
 export class DefaultModule { }
