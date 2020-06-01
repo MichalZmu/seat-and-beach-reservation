54c868314f7c42afd20c233a16f7ab9b30b553ec
Lukasz Hapek <lukaszhapek@gmail.com>
2020-06-01 21:46:44 +0200

    choose place component part 2

--- src/app/modules/reservation/choose-place/choose-place.component.ts
+++ src/app/modules/reservation/choose-place/choose-place.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-choose-place',
@@ -12,104 +13,270 @@ export class ChoosePlaceComponent implements OnInit {
   hourFrom;
   hourTo;
 
-  places: { id: number, reservationDates:[]}[] = [
+  user: {
+    id: string,
+    reservation: [
+      {
+        date: string,
+        hourFrom: string,
+        hourTo: string
+        placeId: number;
+      }
+    ]
+  };
+
+
+  places: { id: number, reservation: [{date: string, userId: string}]}[] = [
     {
-      "id": 1, "reservationDates": []
+      id: 1, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 2, "reservationDates": []
+      id: 2, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 3, "reservationDates": []
+      id: 3, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 4, "reservationDates": []
+      id: 4, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 5, "reservationDates": []
+      id: 5, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 6, "reservationDates": []
-    },  
-    {
-      "id": 7, "reservationDates": []
+      id: 6, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 8, "reservationDates": []
+      id: 7, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 9, "reservationDates": []
+      id: 8, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 10, "reservationDates": []
+      id: 9, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 11, "reservationDates": []
+      id: 10, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 12, "reservationDates": []
+      id: 11, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 13, "reservationDates": []
+      id: 12, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 14, "reservationDates": []
+      id: 13, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 15, "reservationDates": []
+      id: 14, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 16, "reservationDates": []
+      id: 15, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 17, "reservationDates": []
+      id: 16, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 18, "reservationDates": []
+      id: 17, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 19, "reservationDates": []
+      id: 18, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 20, "reservationDates": []
+      id: 19, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 21, "reservationDates": []
+      id: 21, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 22, "reservationDates": []
+      id: 22, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 23, "reservationDates": []
+      id: 23, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 24, "reservationDates": []
+      id: 24, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 25, "reservationDates": []
+      id: 25, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 26, "reservationDates": []
+      id: 26, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 27, "reservationDates": []
+      id: 27, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     },
     {
-      "id": 28, "reservationDates": []
+      id: 28, reservation: [
+        {
+          date: '',
+          userId: ''
+        }
+      ]
     }
-                  
-]
- 
- 
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   isDataChoosed(): void {
-   if(this.hourTo && this.hourFrom && this.reservationDate) {
+   if (this.hourTo && this.hourFrom && this.reservationDate) {
      this.dataChoosed = true;
    }
    console.log(this.places);
+  }
+
+  choosePlace(placeId): void {
+
+    const place = this.places.find(x => x.id === placeId);
+    place.reservation.push({
+      date: this.reservationDate,
+      userId: '1'
+    });
+    console.log(this.places);
+    console.log(this.reservationDate, this.hourFrom, this.hourTo);
+    this.places.push(this.reservationDate, this.hourFrom, this.hourTo, placeId);
+    console.log(this.places);
+  }
+
+  isPlaceAvaiable(id): boolean {
+    const place = this.places.find(el => el.id === id);
+    place.reservation.forEach(date => {
+      if (this.reservationDate === date.date) {
+        return false;
+      }
+    });
+    return true;
   }
 }
